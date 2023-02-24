fb4779659ce2253b26fec2c71698b32f4c364615
iorjunior <juniorcoda@gmail.com>
2023-02-24 08:22:52 -0300

    feat(models): add User model

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_login import UserMixin
 
 
 db = SQLAlchemy()
@@ -13,8 +13,14 @@ def configure(app):
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    directory = db.Column(db.String(255))
-    command = db.Column(db.Text)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    directory = db.Column(db.String(255), nullable=False)
+    command = db.Column(db.Text, nullable=False)
+    token = db.Column(db.String(40), nullable=False)
     last_run = db.Column(db.DateTime)
 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
