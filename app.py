74edf050d1181c56ca7181cb03df261b73ebcbfc
iorjunior <juniorcoda@gmail.com>
2023-02-24 08:21:11 -0300

    build(lib): add flask-login

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 
 from models.models import configure as config_db
 from models.serializers import configure as config_ma
-
+from services.auth import configure as config_auth
 
 def create_app():
     app = Flask(__name__)
@@ -13,11 +13,15 @@ def create_app():
 
     config_db(app)
     config_ma(app)
+    config_auth(app)
 
-    Migrate(app, app.db)
+    Migrate(app, app.db, render_as_batch=True)
 
     from services.tasks import tasks_blueprint
     app.register_blueprint(tasks_blueprint, url_prefix='/tasks')
+
+    from services.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
 
