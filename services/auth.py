081aa250560fdcd8bf04d2293a9c2d3b738b9602
iorjunior <juniorcoda@gmail.com>
2023-02-25 14:02:11 -0300

    fix(auth): add auth logic

--- services/auth.py
+++ services/auth.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify, current_app, request
-from flask_login import LoginManager, login_user, logout_user
+from flask import Blueprint, request, redirect, url_for, render_template
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 from models.models import User
+from datetime import timedelta
 
 login_manager = LoginManager()
 
@@ -13,17 +14,32 @@ def configure(app):
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@auth_blueprint.route("/login", methods=['GET'])
+@auth_blueprint.route("/login", methods=['GET', 'POST'])
 def login():
-    return 200
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.verify_password(password):
+            login_user(user=user, remember=True, duration=timedelta(days=1))
+            return redirect(url_for('main.main'))
+
+        error = True
+
+    return render_template('login.html', error=error)
 
 
 @auth_blueprint.route("/logout", methods=['GET'])
+@login_required
 def logout():
     logout_user()
-    return 200
+    return redirect(url_for('auth.login'))
