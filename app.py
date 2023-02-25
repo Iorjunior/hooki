53899f89368ec1194186052398ec24f54f0034fb
iorjunior <juniorcoda@gmail.com>
2023-02-25 14:04:01 -0300

    feat(app): add PREFIX to url

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from environs import Env
 from flask_migrate import Migrate
 
 from models.models import configure as config_db
 from models.serializers import configure as config_ma
 from services.auth import configure as config_auth
 
-def create_app():
-    app = Flask(__name__)
+env = Env()
+env.read_env()
 
-    app.config['SECRET_KEY'] = 'dasdas'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hooki.db"
+SECRET_KEY = env("SECRET_KEY")
+URL_PREFIX = env("URL_PREFIX")
+DB_URL = env("DB_URL")
+
+
+def create_app():
+    app = Flask(__name__, static_url_path=f'{URL_PREFIX}/static')
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 
     config_db(app)
     config_ma(app)
@@ -18,13 +26,13 @@ def create_app():
     Migrate(app, app.db, render_as_batch=True)
 
     from services.tasks import tasks_blueprint
-    app.register_blueprint(tasks_blueprint, url_prefix='/tasks')
+    app.register_blueprint(tasks_blueprint, url_prefix=f'{URL_PREFIX}/tasks')
 
     from services.auth import auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix=f'{URL_PREFIX}/')
 
     from services.main import main_blueprint
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint, url_prefix=f'{URL_PREFIX}/')
 
     return app
 
